76011f161687ad6bec22c971aca03aee93ccdef1
anirban <debanirban89@gmail.com>
2017-07-07 02:04:36 +0530

    1st user-service code base pushed
    Signed-off-by: anirban <debanirban89@gmail.com>

---
+++ user-service/src/main/java/org/fasttrack/core/ApplicationResource.java
@@ -0,0 +1,12 @@
+package org.fasttrack.core;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationResource {
+
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ApplicationResource.class, args);
+	}
+}
